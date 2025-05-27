2dace7bf1551115394bf6dd9931b6855c51c1eff
Agorrin06 <anthonygorrindo06@gmail.com>
2025-05-27 13:47:42 -0700

    Quiz 6-1: Question 2

--- main.hpp
+++ main.hpp
@@ -1,8 +1,39 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+void getTwoValues(int &begin, int &end);
+bool isPrime(int n);
+int getNextPrime(int begin);
+int getPrevPrime(int end);
 
-// ******************************
-// Implement all your functions here
-// ******************************
+void getTwoValues(int &begin, int &end) {
+    do {
+        cout << "Enter two integers (begin < end): ";
+        cin >> begin >> end;
+    } while (begin >= end);
+}
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int getNextPrime(int begin) {
+    int candidate = begin + 1;
+    while (!isPrime(candidate)) {
+        candidate++; 
+    }
+    return candidate;
+}
+
+int getPrevPrime(int end) {
+    int candidate = end - 1;
+    while (candidate > 1 && !isPrime(candidate)) {
+        candidate--;
+    }
+    return candidate;
+}
